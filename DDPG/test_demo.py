dc99c721a76e2f349f8afd9a6e16e3ac17db106a
Yandong Liu <yd.liu@siat.ac.cn>
2020-05-18 16:55:16 +0800

    Update test_demo.py

--- DDPG/test_demo.py
+++ DDPG/test_demo.py
@@ -23,10 +23,13 @@ def play(env, actor):
     env.close()
 
 if __name__ == '__main__':
+    # 1.环境
     env = gym.make('Pendulum-v0')
     s_dim = env.observation_space.shape[0]
     a_dim = env.action_space.shape[0]
     a_bound = env.action_space.high
+    # 2.模型
     ddpg = DDPG(env, s_dim, a_dim, a_bound)
+    # 3.加载参数
     ddpg.load_ckpt()
     play(env, ddpg.actor)
